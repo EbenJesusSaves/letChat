0a6357f5f0b537aede1afa4e37df461724aa9886
Ebenezer <agyekumebenezer2001@gmail.com>
2023-04-18 08:32:30 +0000

    finally fixed the big Bug

--- utils/actions/userActions.js
+++ utils/actions/userActions.js
@@ -1,48 +1,65 @@
 import { child, get, getDatabase, ref } from "firebase/database";
-import React from "react";
+
 import { app } from "../../Auth/firebase/firebaseConfig";
 
-// export const getUserData = async (userId) => {
-//   try {
-//     const dbRef = ref(getDatabase(app));
-//     const userRef = child(dbRef, `users/${userId}`);
-
-//     const snapshot = await get(userRef);
-//     console.log(snapshot.val() + "hi");
-//     return snapshot.val();
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-//so this function don't even run
 export const getUserData = async (userId) => {
   try {
-    console.log(" I dont run");
-    // const app = getFirebaseApp();
-    console.log(userId);
     const dbRef = ref(getDatabase());
-    const userRef = get(child(dbRef, `users/${userId}`));
-    console.log(userRef);
-    const snapshot = await userRef;
-    console.log(snapshot.val() + "hi😂😂");
+    const userRef = child(dbRef, `users/${userId}`);
 
+    const snapshot = await get(userRef);
+    console.log(snapshot.val() + "hi");
     return snapshot.val();
-
-    // const dbRef = ref(getDatabase(app));
-    // get(child(dbRef, `users/${userId}`))
-    //   .then((snapshot) => {
-    //     if (snapshot.exists()) {
-    //       console.log(snapshot.val());
-    //       return snapshot.val();
-    //     } else {
-    //       console.log("No data available");
-    //     }
-    //   })
-    //   .catch((error) => {
-    //     console.error(error);
-    //   });
   } catch (error) {
     console.log(error);
   }
 };
+
+//so this function don't even run
+// export const getUserData = async (userId) => {
+//   // try {
+//   //   // console.log(" I dont run");
+//   //   // // const app = getFirebaseApp();
+//   //   // console.log(userId);
+//   //   // const dbRef = ref(getDatabase());
+//   //   // const userRef = get(child(dbRef, `users/${userId}`));
+//   //   // console.log(userRef);
+//   //   // const snapshot = await userRef;
+//   //   // console.log(snapshot.val() + "hi😂😂");
+//   //   // return snapshot.val();
+
+//   //   const dbRef = ref(getDatabase());
+//   //   get(child(dbRef, `users/${userId}`))
+//   //     .then((snapshot) => {
+//   //       if (snapshot.exists()) {
+//   //         console.log(snapshot.val());
+//   //         return snapshot.val();
+//   //       } else {
+//   //         console.log("No data available");
+//   //       }
+//   //     })
+//   //     .catch((error) => {
+//   //       console.error(error);
+//   //     });
+//   // } catch (error) {
+//   //   console.log(error);
+//   // }
+
+//   console.log(userId, "just checking how this works ");
+
+//   const dbRef = ref(getDatabase());
+//   get(child(dbRef, `users/${userId}`))
+//     .then((snapshot) => {
+//       if (snapshot.exists()) {
+//         console.log(snapshot.val() && snapshot.val());
+//         return snapshot.val() && snapshot.val();
+//       } else {
+//         console.log("No data available");
+//       }
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//     });
+// };
+
+console.log(getUserData());
