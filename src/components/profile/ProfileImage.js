712a45ed7feedd2ba71987c2c63085900cebec39
Ebenezer <agyekumebenezer2001@gmail.com>
2023-04-27 23:35:18 +0000

    finished the profile picture section

--- src/components/profile/ProfileImage.js
+++ src/components/profile/ProfileImage.js
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import {
+  ActivityIndicator,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import defaultProfile from "../../../assets/images/userImage.jpeg";
 import styled from "styled-components";
 import { EvilIcons } from "@expo/vector-icons";
@@ -18,12 +24,15 @@ export const ProfileImage = (props) => {
   const userId = props.userId;
 
   const [userProfile, setUserProfile] = useState(source);
+  const [isLoading, setIsLoading] = useState(false);
   const pickImage = async () => {
     try {
       const tempProfileUri = await launchImagePicker();
       if (!tempProfileUri) return;
 
+      setIsLoading(true);
       const uploadUri = await uploadImageAsync(tempProfileUri);
+      setIsLoading(false);
 
       const newData = { profilePicture: uploadUri };
 
@@ -31,16 +40,35 @@ export const ProfileImage = (props) => {
       dispatch(updateLoggedInUserData({ newData: newData }));
 
       setUserProfile({ uri: uploadUri });
-    } catch (error) {}
+    } catch (error) {
+      setIsLoading(false);
+    }
   };
 
   return (
     <View>
       <TouchableOpacity onPress={pickImage}>
-        <ProfilePic
-          style={{ height: props.size, width: props.size }}
-          source={userProfile}
-        />
+        {isLoading ? (
+          <View
+            style={{
+              height: 100,
+              width: 100,
+              borderColor: "white",
+              borderWidth: 1,
+              borderRadius: 50,
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <ActivityIndicator size={"small"} />
+          </View>
+        ) : (
+          <ProfilePic
+            style={{ height: props.size, width: props.size }}
+            source={userProfile}
+          />
+        )}
+
         <ChangeProfileIcon name="pencil" size={24} color="white" />
       </TouchableOpacity>
     </View>
